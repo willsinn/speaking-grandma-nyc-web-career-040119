704bbae18efbdaac6c667c41c119ea891cb65904
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 03:45:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,7 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
    end
-   
+   if phrase != phrase.upcase
+     return 'HUH?! SPEAK UP, SONNY!'
+     
 end
